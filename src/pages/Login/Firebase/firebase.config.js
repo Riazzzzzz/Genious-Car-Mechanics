7e46ff0b0672059d4beacc64dbd34c1dd9627b32
Riazzzzzz <130599791+Riazzzzzz@users.noreply.github.com>
2023-05-23 23:41:34 +0600

    Genious Car Mechanics
    Website developed with React & JavaScript including Backend properties.

---
+++ src/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+console.log(process.env);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZXoiZdxYPqn_Wz1pajrc6kfm_cJbVbPA",
+  authDomain: "automation-car-service.firebaseapp.com",
+  projectId: "automation-car-service",
+  storageBucket: "automation-car-service.appspot.com",
+  messagingSenderId: "930846079521",
+  appId: "1:930846079521:web:d3be4e477fc6ae1f6a4160"
+};
+
+export default firebaseConfig;
+
+// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
